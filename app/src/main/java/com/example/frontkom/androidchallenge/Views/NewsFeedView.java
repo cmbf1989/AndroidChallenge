0942095aec0642950272f7b90dcf5aff89b7a396
Cesar <cmbf1989@gmail.com>
2018-02-08 20:33:01 +0000

    theme changes. layout recycledview item. layout news feed. layout news menu

--- app/src/main/java/com/example/frontkom/androidchallenge/Views/NewsFeedView.java
+++ app/src/main/java/com/example/frontkom/androidchallenge/Views/NewsFeedView.java
@@ -3,6 +3,7 @@ package com.example.frontkom.androidchallenge.Views;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 import com.example.frontkom.androidchallenge.R;
 
@@ -12,7 +13,14 @@ public class NewsFeedView extends AppView {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_feed_view);
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar myToolbar = findViewById(R.id.toolbar);
         setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
